8c63649be046076e95b8637ae16cf6d7638a9a82
son.tran <son.tran@team.enouvo.com>
2023-11-16 20:07:17 +0700

    feat(master): modifer api

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -7,7 +7,7 @@ export const config = {
 
 const app = new Hono().basePath('/api');
 
-app.get('/sign-in', (c) => c.json({ token: '124' }));
-app.get('/sign-up', (c) => c.json({ token: '124' }));
+app.post('/sign-in', (c) => c.json({ token: '124' }));
+app.post('/sign-up', (c) => c.json({ token: '124' }));
 
 export default handle(app);
