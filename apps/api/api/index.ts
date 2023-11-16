198fd58feec3f40413000162fb497b08bd059b6e
son.tran <son.tran@team.enouvo.com>
2023-11-16 20:03:14 +0700

    feat(master): add api sign-up

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -8,5 +8,6 @@ export const config = {
 const app = new Hono().basePath('/api');
 
 app.get('/sign-in', (c) => c.json({ token: '124' }));
+app.get('/sign-up', (c) => c.json({ token: '124' }));
 
 export default handle(app);
