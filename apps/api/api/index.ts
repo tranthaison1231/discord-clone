10317af14de58521a1fda5c897ee81b19078e518
son.tran <son.tran@team.enouvo.com>
2023-11-16 20:13:40 +0700

    feat(master): add logger

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,11 +1,22 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
 
 export const config = {
   runtime: 'edge',
 };
 
 const app = new Hono().basePath('/api');
+
+app.use('*', logger());
+app.use(
+  '*',
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  })
+);
 
 app.post('/sign-in', (c) => c.json({ token: '124' }));
 app.post('/sign-up', (c) => c.json({ token: '124' }));
