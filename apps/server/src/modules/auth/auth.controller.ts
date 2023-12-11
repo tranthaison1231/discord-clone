7ca72941ce83faaa41b98583f424db3e06bf4782
son.tran <son.tran@team.enouvo.com>
2023-12-11 20:00:20 +0700

    feat(api): add mail service

--- apps/server/src/modules/auth/auth.controller.ts
+++ apps/server/src/modules/auth/auth.controller.ts
@@ -1,17 +1,19 @@
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
 import { AuthService } from "./auth.service";
+import { signUpDto, signInDto, forgotPasswordDto } from "./dto/auth.dto";
 
 export const router = new Hono();
 
 router
-  .post("/sign-in", async (c) => {
+  .post("/sign-in", zValidator("json", signInDto), async (c) => {
     const { email, password } = await c.req.json();
 
     const data = await AuthService.signIn(email, password);
 
     return c.json(data, 200);
   })
-  .post("/sign-up", async (c) => {
+  .post("/sign-up", zValidator("json", signUpDto), async (c) => {
     const { email, password } = await c.req.json();
 
     await AuthService.signUp(email, password);
@@ -23,4 +25,21 @@ router
       },
       201
     );
-  });
+  })
+  .post(
+    "/forgot-password",
+    zValidator("json", forgotPasswordDto),
+    async (c) => {
+      const { email } = await c.req.json();
+
+      await AuthService.forgotPassword(email);
+
+      return c.json(
+        {
+          message:
+            "We have sent a password reset link to your email. Please check your email.",
+        },
+        201
+      );
+    }
+  );
--- apps/server/src/modules/auth/auth.service.ts
+++ apps/server/src/modules/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { hashPassword } from "@/utils/password";
 import * as bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@/lib/config";
+import { mailService } from "@/lib/mail.service";
 
 export const ACCESS_TOKEN_EXPIRE_IN = 60 * 60;
 
@@ -25,9 +26,7 @@ export class AuthService {
       throw new UnauthorizedException(`Invalid password`);
     }
 
-    const accessToken = jwt.sign({ userId: user.id }, JWT_SECRET, {
-      expiresIn: ACCESS_TOKEN_EXPIRE_IN,
-    });
+    const accessToken = this.createToken(user.id);
 
     return { accessToken };
   }
@@ -60,5 +59,29 @@ export class AuthService {
         throw error;
       }
     }
+  }
+
+  static createToken(userId: string) {
+    return jwt.sign({ userId: userId }, JWT_SECRET, {
+      expiresIn: ACCESS_TOKEN_EXPIRE_IN,
+    });
+  }
+
+  static async forgotPassword(email: string) {
+    const user = await db.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (!user) {
+      throw new BadRequestException(`Email ${email} not found`);
+    }
+    const accessToken = this.createToken(user.id);
+
+    await mailService.sendMail({
+      to: email,
+      html: `Click <a href="http://localhost:3000/auth/reset-password?token=${accessToken}">here</a> to reset your password`,
+      subject: "Reset  password",
+    });
   }
 }
