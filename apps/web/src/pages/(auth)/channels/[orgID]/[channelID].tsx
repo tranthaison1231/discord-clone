faf50f56e8414ef986c426711f638d7bdc66865f
son.tran <son.tran@team.enouvo.com>
2024-01-23 21:52:54 +0700

    feat(master): update channel

--- apps/web/src/pages/(auth)/channels/[orgID]/[channelID].tsx
+++ apps/web/src/pages/(auth)/channels/[orgID]/[channelID].tsx
@@ -1,15 +1,15 @@
-import TooltipIcon from '@/components/TooltipIcon';
-import { cn } from '@/lib/utils';
-import { useParams } from '@/router';
-import { Frame, HelpCircle, Users2 } from 'lucide-react';
-import { useState } from 'react';
-import ChatList from './_components/ChatList';
-import MemberList from './_components/MemberList';
-import PopoverInbox from './_components/PopoverInbox';
+import TooltipIcon from "@/components/TooltipIcon";
+import { cn } from "@/lib/utils";
+import { useParams } from "@/router";
+import { Frame, HelpCircle, Users2 } from "lucide-react";
+import { useState } from "react";
+import ChatList from "./_components/ChatList";
+import MemberList from "./_components/MemberList";
+import PopoverInbox from "./_components/PopoverInbox";
 
 export default function Channel() {
   const [isHideMemberList, setIsHideMemberList] = useState(false);
-  const { channelID } = useParams('/channels/:orgID/:channelID');
+  const { channelID } = useParams("/channels/:orgID/:channelID");
   return (
     <div>
       <header className="p-3 h-14 border-b bg-primary-foreground/20 text-2xl flex items-center justify-between">
@@ -20,13 +20,16 @@ export default function Channel() {
         <div className="flex items-center gap-4">
           <TooltipIcon
             icon={
-              <Users2 className={cn("cursor-pointer text-gray-400 hover:text-white", {
-                "text-white": !isHideMemberList
-              })} onClick={() => setIsHideMemberList(!isHideMemberList)} />
+              <Users2
+                className={cn("cursor-pointer text-gray-400 hover:text-white", {
+                  "text-white": !isHideMemberList,
+                })}
+                onClick={() => setIsHideMemberList(!isHideMemberList)}
+              />
             }
             content={isHideMemberList ? "Show member list" : "Hide member list"}
           />
-          <PopoverInbox  />
+          <PopoverInbox />
           <HelpCircle className="cursor-pointer text-gray-400 hover:text-white" />
         </div>
       </header>
