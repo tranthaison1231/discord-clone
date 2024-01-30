99ba9f7e60597fabc3f88463cf6533d070ada4e5
son.tran <son.tran@team.enouvo.com>
2024-01-30 19:32:32 +0700

    feat(master): modified create org

--- apps/server/src/modules/orgs/dto/create-org.dto.ts
+++ apps/server/src/modules/orgs/dto/create-org.dto.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const createOrgDto = z.object({
   name: z.string().min(1, "Name is required"),
-  icon: z.string().min(1, "Icon is required"),
+  icon: z.string(),
 });
